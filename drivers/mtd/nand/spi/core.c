d39f0d5442637b1e94123382fd2000ca11736f52
MS10 CWWeng <cwweng@nuvoton.com>
2025-11-06 15:01:38 +0800

    Add MK Founder MKSV2GIL-AA SPI NAND support
    Change-Id: Ib5b84b8ff76fd4656725eeca957000a491b69a97

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -858,6 +858,7 @@ static const struct spinand_manufacturer *spinand_manufacturers[] = {
 	&micron_spinand_manufacturer,
 	&toshiba_spinand_manufacturer,
 	&winbond_spinand_manufacturer,
+	&mkfd_spinand_manufacturer,
 };
 
 static int spinand_manufacturer_detect(struct spinand_device *spinand)
--- include/linux/mtd/spinand.h
+++ include/linux/mtd/spinand.h
@@ -218,6 +218,7 @@ extern const struct spinand_manufacturer macronix_spinand_manufacturer;
 extern const struct spinand_manufacturer micron_spinand_manufacturer;
 extern const struct spinand_manufacturer toshiba_spinand_manufacturer;
 extern const struct spinand_manufacturer winbond_spinand_manufacturer;
+extern const struct spinand_manufacturer mkfd_spinand_manufacturer;
 
 /**
  * struct spinand_op_variants - SPI NAND operation variants
